f00441ca14709dd46df0b7aca4faffa4a43b0a53
IgorPestunov <67595885+IgorPestunov@users.noreply.github.com>
2020-06-29 23:01:54 +0700

    Initial commit

---
+++ Lab4/Models/ResetModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class ResetModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ Lab4/Models/SignUpModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class SignUpModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Day { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public string Gender { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password not the same.")]
+        public string Confirm { get; set; }
+    }
+}
